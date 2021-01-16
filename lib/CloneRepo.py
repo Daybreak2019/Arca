0e01d3016d30739270e64439b9dd5e3a6e9b5e3a
Daybreak2019 <li.wen@wsu.edu>
2021-01-15 21:14:40 -0800

    code snippet summary

--- lib/CloneRepo.py
+++ lib/CloneRepo.py
@@ -7,24 +7,41 @@ import os
 import requests
 import pandas as pd
 from time import sleep
+import re
+import nltk
+nltk.download('stopwords');nltk.download('brown');nltk.download('punkt');nltk.download('wordnet')
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+
 
 class Diff ():
     def __init__(self, file, content):
         self.file = file
         self.content = content
 
+    def AddFile (self, file):
+        self.file += " " + file
+
+    def AddContent (self, content):
+        self.content += " " + content
+
 class Commit ():
     def __init__(self, id, sha, author, date, message):
         self.id      = id
         self.sha     = sha
-        self.author	 = author
-        self.date	 = date
+        self.author  = author
+        self.date    = date
         self.message = message
 
-        self.Diffs   = []
+        self.Diffs   = None
 
     def AddDiff (self, DF):
-        self.Diffs.append (DF)
+        if self.Diffs == None:
+            self.Diffs = DF
+        else:
+            self.Diffs.AddFile(DF.file)
+            self.Diffs.AddContent(DF.content)
         
 
 class CloneRepo():
@@ -41,41 +58,37 @@ class CloneRepo():
                      '.hpp', '.jsp', '.pas', '.phtml', '.s', '.vbs']
         self.BaseDir = os.getcwd ()
 
-    def GetCmmtDir (self, RepoId):
-        RepoCmmtDir = self.BaseDir + "/Data/CmmtSet/" + str (RepoId) + "/"
-        if not os.path.exists (RepoCmmtDir):
-            os.mkdir (RepoCmmtDir)
-        return RepoCmmtDir
-
-
-    def WriteContent (self, CmmtDir, Cmmt):
-        CntDir = CmmtDir + "Content/"
-        if not os.path.exists (CntDir):
-            os.mkdir (CntDir)
+    def CleanText(self, Text):
+        Text = str (Text)
+        Text = re.sub(r'[+|/]', ' and ', Text)
+        Text = re.sub(r'[^\w\d,.]', ' ', Text)
+        Text = Text.lower()
+        words = Text.split()
+        words = [re.sub(r'[^a-z]', '', word) for word in words if word.isalnum()]
+        Text = ' '.join(words)
+        return Text      
         
-        FileName = CntDir + str (Cmmt.id) + ".csv"
-        Header = ['file', 'content']
-        with open(FileName, 'w', encoding='utf-8') as CsvFile:       
-            writer = csv.writer(CsvFile)
-            writer.writerow(Header)  
-            for Dif in Cmmt.Diffs:
-                row = [Dif.file, Dif.content]
-                writer.writerow(row)
-        CsvFile.close()
+    def Cleaning(self, Text, min_len=4, max_len=16):
+        Text = self.CleanText (Text)       
+        words = nltk.word_tokenize(Text)
+        words = [lemmatizer.lemmatize(word) for word in words]
+        stopwords_list = stopwords.words('english') 
+        words = [word for word in words if word not in stopwords_list and len(word) > min_len and len(word) < max_len]
+        words = list (set (words))
+        return " ".join(words)
 
     def WriteCommts (self, RepoId):
-        RepoCmmtDir = self.GetCmmtDir (RepoId)
-        CmmtFile = RepoCmmtDir + str (RepoId) + ".csv"
-        Header = ['id', 'sha', 'author', 'date', 'message']
+        CmmtFile = self.BaseDir + "/Data/CmmtSet/" + str (RepoId) + ".csv"
+        Header = ['id', 'sha', 'author', 'date', 'message', 'file', 'content']
         with open(CmmtFile, 'w', encoding='utf-8') as CsvFile:       
             writer = csv.writer(CsvFile)
             writer.writerow(Header)  
             for cmmt in self.Commits:
-                row = [cmmt.id, cmmt.sha, cmmt.author, cmmt.date, cmmt.message]
+                if cmmt.Diffs == None:
+                    row = [cmmt.id, cmmt.sha, cmmt.author, cmmt.date, cmmt.message, "", ""]
+                else:
+                    row = [cmmt.id, cmmt.sha, cmmt.author, cmmt.date, cmmt.message, cmmt.Diffs.file, cmmt.Diffs.content]
                 writer.writerow(row)
-
-                if len (cmmt.Diffs) != 0:
-                    self.WriteContent (RepoCmmtDir, cmmt)
         CsvFile.close()
 
     def is_continue (self, errcode):
@@ -159,9 +172,12 @@ class CloneRepo():
             Df      = None
             DfContent = ""
             for line in Lfile:
+                if line[0:2] in ["- ", "@@",  "--", "++", "in"]:
+                    continue
+                
                 if line[0:7] == "commit ":
                     if Df != None:
-                        Df.content = DfContent
+                        Df.content = self.Cleaning(DfContent) 
                         Cmmt.AddDiff (Df)
                         #print (DfContent)
                         Df = None
@@ -195,7 +211,7 @@ class CloneRepo():
                     if state == 2:
                         if line[0:12] == "diff --git a":
                             if Df != None:
-                                Df.content = DfContent
+                                Df.content = self.Cleaning(DfContent)
                                 Cmmt.AddDiff (Df)
                                 Df = None
                                 DfContent = ""
@@ -220,6 +236,7 @@ class CloneRepo():
         LogCmd = "git log -20000 --date=iso -p > " + LogFile
         os.system (LogCmd)
         print (LogCmd)
+        print ("ParseLog....")
         self.ParseLog (LogFile)
         self.WriteCommts (RepoId)
         os.remove (LogFile)
@@ -256,6 +273,6 @@ class CloneRepo():
             self.CloneLog (repo['id'], RepoDir)
             System.set_tag (str(repo['id']))
             self.Clean (RepoDir)
-
+ 
 
     
